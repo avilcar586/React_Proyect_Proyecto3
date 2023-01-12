4aa2bc4472ec531c5fe00a624e213c175a3c3a13
Antonio Vilches-Casa <avilcar586@g.educaand.es>
2023-01-12 15:53:50 +0100

    Añadido menu base, por editar

---
+++ src/components/header/Header.js
@@ -0,0 +1,17 @@
+import Nav from "./Nav";
+
+const Header = () => {
+
+        return (
+        
+        <>
+        <h1>Header</h1>
+
+        <div>
+            <Nav />
+        </div>
+        </>
+    )
+}
+
+export default Header;
